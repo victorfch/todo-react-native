143e1ee6d50caf1b4ea0d195fea8983ef3d85cd9
VictorFCh <vfariachavez@gmail.com>
2021-01-04 19:18:21 +0000

    added style to list component

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,11 +1,35 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, StyleSheet, View } from "react-native";
+
+const styles = StyleSheet.create({
+  listItem: {
+    borderWidth: 1,
+    margin: 5,
+    width: "80%",
+  },
+  text: {
+    padding: 5,
+    fontWeight: "bold",
+  },
+  textDone: {
+    textDecorationLine: "line-through",
+    textDecorationStyle: "solid",
+    color: "#aaa",
+  },
+});
 
 const TodoList = ({ todos }) => (
   <>
-    {todos.map(
-      (todo) => !todo.done && <Text key={todo.text}>{todo.text}</Text>
-    )}
+    {todos.map((todo) => (
+      <View style={styles.listItem}>
+        <Text
+          style={[styles.text, todo.done && styles.textDone]}
+          key={todo.text}
+        >
+          {todo.text}
+        </Text>
+      </View>
+    ))}
   </>
 );
 
