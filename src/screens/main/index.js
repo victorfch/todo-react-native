dd2f0a45cd884b640d1506d5d5ad8980d1a917b7
VictorFCh <vfariachavez@gmail.com>
2021-01-04 02:56:44 +0000

    changed main screen js to jsx

--- src/screens/main/index.js
+++ src/screens/main/index.js
@@ -1,21 +1,3 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-
-const MainScreen = () => {
-    return (
-        <View style={styles.container}>
-            <Text>Hola mundo! - componente</Text>
-        </View>
-    );
-};
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
+import MainScreen from './MainScreen';
 
 export default MainScreen;
